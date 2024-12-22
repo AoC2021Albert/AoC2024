d43dc0381e649017767b2024d844579dabb00c9a
Albert Bendicho <albert.bendicho@gmail.com>
2024-12-22 09:12:44 +0000

    Day 22 part 2

--- 22/22.py
+++ 22/22.py
@@ -10,6 +10,7 @@ from copy import deepcopy
 
 f = open('22/in.raw', 'r')
 #f = open('22/sample.raw', 'r')
+#f = open('22/sample2.raw', 'r')
 lines = f.read().splitlines()
 
 res=0
@@ -23,3 +24,33 @@ for num in map(int, lines):
         num %= (1 << 24)
     res+=num
 print(res)
+
+
+res=0
+seq_value=defaultdict(int)
+for num in map(int, lines):
+    ready = False
+    seq = []
+    prev = math.inf
+    local_seq_value=dict()
+    for _ in range(2000):
+        num ^= num << 6
+        num %= (1 << 24)
+        num ^= num >> 5
+        num %= (1 << 24)
+        num ^= num << 11
+        num %= (1 << 24)
+        value= num %10
+        diff = value - prev
+        seq.append(diff)
+        if tuple(seq[-4:]) not in local_seq_value:
+            local_seq_value[tuple(seq[-4:])] = value
+        prev = value
+    for k,v in local_seq_value.items():
+        seq_value[k]+=v
+    res+=num
+print(res)
+
+print(sorted([v for k,v in seq_value.items() if len(k)==4])[-1])
+
+#7372 too high
